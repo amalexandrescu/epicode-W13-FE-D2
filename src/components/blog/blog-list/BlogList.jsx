215f552d90de741e67014bddfe6f0587060e1fa9
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-01-10 17:05:10 +0200

    creates fetching from the url and adds the reatc_app_be_url in the .env file

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
+// import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   async function getBlogPosts() {
+  //     try {
+  //       let req = await fetch("http://localhost:3001/blogposts");
+  //       if (req.ok) {
+  //         let blogPosts = await req.json();
+  //         setPosts(blogPosts);
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   getBlogPosts();
+  // }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const apiUrl = process.env.REACT_APP_BE_URL;
+      const resp = await fetch(`${apiUrl}/blogPosts`);
+      const data = await resp.json();
+      setPosts(data);
+    }
+    fetchData();
+  }, []);
   return (
     <Row>
       {posts.map((post) => (
