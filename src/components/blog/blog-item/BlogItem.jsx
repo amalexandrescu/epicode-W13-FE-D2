68bd5a07c1e5579377e0316ba2b53e9bb25a8af0
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-01-12 17:55:16 +0200

    adds button for downloading pdf file

--- src/components/blog/blog-item/BlogItem.jsx
+++ src/components/blog/blog-item/BlogItem.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import BlogAuthor from "../blog-author/BlogAuthor";
 import "./styles.css";
 const BlogItem = (props) => {
   const { title, cover, author, _id } = props;
   return (
-    <Link to={`/blog/${_id}`} className="blog-link">
-      <Card className="blog-card">
-        <Card.Img variant="top" src={cover} className="blog-cover" />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <BlogAuthor {...author} />
-        </Card.Footer>
-      </Card>
-    </Link>
+    <>
+      {" "}
+      <a download href="http://localhost:3001/blogPosts/blogsPdf">
+        <Button>pdf download</Button>
+      </a>
+      <Link to={`/blog/${_id}`} className="blog-link">
+        <Card className="blog-card">
+          <Card.Img variant="top" src={cover} className="blog-cover" />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+          </Card.Body>
+          <Card.Footer>
+            <BlogAuthor {...author} />
+          </Card.Footer>
+        </Card>
+      </Link>
+    </>
   );
 };
 
